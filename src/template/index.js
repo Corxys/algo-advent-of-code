f222b98434ee87a291aad5c0415121c32d0bac46
jlboulanger <jennylee.boulanger@caldera.com>
2022-12-16 00:01:01 +0100

    :wrench: template advent of code

---
+++ src/template/index.js
@@ -0,0 +1,6 @@
+const readInput = require("../../src/utils/readInput.js");
+
+const prepareInput = (rawInput) => rawInput;
+
+const input = prepareInput(readInput({"input": "input.txt"}));
+console.log(input);
